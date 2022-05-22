3402fc893ea6b50b8841a9acb9a1d557662cb749
Al Capitan <alexandre.boyer29@gmail.com>
2022-05-22 19:41:27 +0200

    Ajout de fonctionnalités vers l'optimalité en command-line

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# battleship
-Battleship game using Python (with Pygame)
+# Battleship
+
+Jeu de bataille navale.
+
+Codé en Python, utilise la librairie Pygame.
+Dévellopé par Alexandre BOYER et Florentin LIU, dans le cadre d'un projet en NSI.
--- back.py
+++ back.py
@@ -1,35 +1,88 @@
 from random import randint
 
-table = [[0 for j in range(10)] for i in range(10)]
+def createEmptyTable(size):
+     """The table is a square."""
+     table = [[0 for x in range(size)] for y in range(size)]
+     return table
 
-def displayTable(table):
-    for i in range(len(table)):
-        print(table[i])
-    print()
+def displayTable(table,view):
+     """
+          Display the table in command-line mode.
+          View option displays boats that one player should be able to see.
+     """
+     for y in range(len(table)):
+          for x in range(len(table)):
+               if view == None:
+                    print(f"{table[y][x]} ")
+               elif table[y][x] == view:
+                    print(f"{view} ")
+               elif table[y][x] != view:
+                    print("0 ")
+               else:
+                    return None
+     print()
 
-def putBoatRandomly(table):
-    for player in range(1,3):
-        boatNb = 0
-        while boatNb < 10:
-            x = randint(0,9)
-            y = randint(0,9)
-            if table[x][y] == 0:
-                table[x][y] = player
-                boatNb += 1
-    return table
+def prepareTable(size,nbBoats):
+     """Create an empty table, then put boats on the table."""
+     table = createEmptyTable(size)
+     for player in range(1,3):
+          boats = 0
+          while boats < nbBoats:
+               x = randint(0,nbBoats-1)
+               y = randint(0,nbBoats-1)
+               if table[x][y] == 0:
+                    table[x][y] = player
+                    boats += 1
+     return table
 
-putBoatRandomly(table) # Put
-displayTable(table) # Start
+def boatHere(table,player,position):
+     """
+          table is table of the play
+          player is 1 or 2 according to user identifiant (who call this function)
+          position is a tuple. First is x (left - right), and second is y (top - bottom)
+          Remember in Python, lists start from 0. So remember to move 1 from the position !
+     """
+     x = position[0]
+     y = position[1]
+     if (table[y][x] != player) and (table[y][x] != 0):
+          return "opposite"
+     elif table[y][x] == player:
+          return "same"
+     elif table[y][x] == 0:
+          return "empty"
+     else:
+          return None
 
-def boatHere(table,player,x,y):
-    # player est l'identifiant (1 ou 2) de celui qui envoie la fonction (= celui qui joue lz tour)
-    if (table[x][y] != player) and (table[x][y] != 0):
-        print("Joueur adverse.")
-    elif table[x][y] == player:
-        print("But contre son camp.")
-    elif table[x][y] == 0:
-         print("Mer.")
-    else:
-         print("Error.")
+def shoot():
+     """Shoot on a position"""
+     x = input("x : ")
+     y = input("y : ")
+     return (x,y)
 
-boatHere(table,1,8,4)
+def destroyBoat(table,player,x,y):
+     """Remove a opposite boat"""
+     if boatHere(table,player,x,y) == "opposite":
+          table[x][y] = 0
+          return "success"
+     else:
+          return "error"
+
+def run():
+     size = input("How size for the grid : ")
+     nbBoats = input("How many boats per player : ")
+     boats = (nbBoats,nbBoats)
+     gameRun = True
+     table = prepareTable(size,nbBoats)
+     player = randint(0,1)
+     while gameRun:
+          print(f"Player {player} is going to play.")
+          displayTable(table,player)
+          position = shoot()
+          result = boatHere(table,player,position)
+          if result == "opposite":
+               print("You have destoyed an opposite boat.")
+          elif result == "same":
+               print("You have destoyed one of your own boats.")
+          elif result == "empty":
+               print("You have not destoyed anything.")
+
